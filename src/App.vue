ad8f23b3192bd933539b09814ed45c3a951e9e9e
GiadaMarzapane <giada.ortesta@gmail.com>
2023-02-08 12:45:31 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+    name: 'App',
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
+
+</template>
+
+<style lang="scss">
+    @use "./style/style.scss" as *
+</style>
